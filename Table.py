119a49c67bc392ed0018f0202755bd1e94c65928
SashankSrivastava <97671874+SashankSrivastava@users.noreply.github.com>
2022-01-16 18:11:22 +0530

    Add files via upload

---
+++ Table.py
@@ -0,0 +1,20 @@
+# PROGRAM TO PRINT TABLE OF ANY GIVEN NUMBER
+
+while(1):
+    print("Hello user, this is MIKE your persnoal assitant, this program will help you learn and know the table of any integer")
+    x = int(input("Enter a Number to print Table : "))
+
+    print("Table for " + str(x) + " is : \n")
+    print(str(x) + " * 1 = " + str(1*x) )
+    print(str(x) + " * 2 = " + str(2*x) )
+    print(str(x) + " * 3 = " + str(3*x) )
+    print(str(x) + " * 4 = " + str(4*x) )
+    print(str(x) + " * 5 = " + str(5*x) )
+    print(str(x) + " * 6 = " + str(6*x) )
+    print(str(x) + " * 7 = " + str(7*x) )
+
+    print(str(x) + " * 8 = " + str(8*x) )
+    print(str(x) + " * 9 = " + str(9*x) )
+    print(str(x) + " * 10 = " + str(10*x) )
+    print("\n\nTHANK YOU FOR USING THIS PROGRAM, MIKE IS ALWAYS HAPPY TO HELP YOU\n\n")
+    
